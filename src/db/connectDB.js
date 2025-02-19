ce7d98948fb4742e813ec7a60c2894a46a92240b
Shanmukha Moka <shanmukhamoka@Shanmukhas-MacBook-Pro.local>
2025-02-19 12:00:30 +0530

    changed connection format

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -2,14 +2,19 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 import "dotenv/config"
-const pool =  new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT
-    });
+// const pool =  new Pool({
+//         user: process.env.DB_USER,
+//         host: process.env.DB_HOST,
+//         database: process.env.DB_NAME,
+//         password: process.env.DB_PASSWORD,
+//         port: process.env.DB_PORT
+// });
 
+const pool =new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false, // Enables SSL for production
+
+})
 pool.connect()
     .then(client => {
         console.log("✅ Connected to PostgreSQL");
